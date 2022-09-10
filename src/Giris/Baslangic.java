31f8c4579e7f3842852d5740ebf72fd8ba7b746f
Asena Yıldız <104993914+H-AsenaYildiz@users.noreply.github.com>
2022-09-10 13:54:33 +0300

    Update Baslangic.java

--- src/Giris/Baslangic.java
+++ src/Giris/Baslangic.java
@@ -1,44 +1,17 @@
 import java.util.Scanner;
 
-public class Baslangic {
+public class KDVhesaplama {
     public static void main(String[] args) {
-
-        // deðiþkenleri oluþtur
-        int mat, fizik, kimya, turkce, tarih, muzik;
-
-        //Scanner sýnýfý tanýmlandý
         Scanner inp = new Scanner(System.in);
 
-        //Kullanýcýdan deðerleri al
-        System.out.print("Matematik notunuzu giriniz: ");
-        mat = inp.nextInt();
-        //System.out.println(mat);
+        System.out.print("KDV'siz Fiyat: ");
 
-        System.out.print("Fizik notunuzu giriniz: : ");
-        fizik =inp.nextInt();
+        Double fiyat= inp.nextDouble();
+        Double kdv=fiyat*18/100;
 
-        System.out.print("Kimya notunuzu giriniz: ");
-        kimya=inp.nextInt();
-
-        System.out.print("Turkce notunuzu giriniz: ");
-        turkce=inp.nextInt();
-
-        System.out.print("Tarih notunuzu giriniz: ");
-        tarih=inp.nextInt();
-
-        System.out.print("Müzik notunuzu giriniz: ");
-        muzik=inp.nextInt();
-
-        double a;
-        a= (mat+muzik+kimya+fizik+tarih+turkce)/6.0;
-
-        System.out.println("Ortalamanýz: " +a);
-
-        Boolean sonuc=a>=60;
-        String str=(sonuc)? "Geçti":"Kaldý";
-        System.out.println(str);
+        System.out.println("KDV'li Fiyat: "+(kdv+fiyat));
+        System.out.println("KDV tutarÄ±: "+kdv);
 
 
     }
-
 }
